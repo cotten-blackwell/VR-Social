94a57158f9b7f5588908db54dd0fed2ddca57610
cotten-blackwell <cotten.blackwell@gmail.com>
2016-02-12 11:17:49 -0800

    Initial commit of files from private project...
    ...to this *public* repo

---
+++ Assets/Scripts/AvatarMultiplayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class AvatarMultiplayer : NetworkBehaviour
+{
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject camera = GameObject.FindWithTag("MainCamera");
+        transform.parent = camera.transform;
+        transform.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/NetworkStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class NetworkStart : MonoBehaviour {
+    public GameObject oculusMain;
+    public GameObject cardboardMain;
+    public string hostIP = "10.0.1.14";
+
+    void Awake()
+    {
+        VRSettings.enabled = false;
+
+        #if (UNITY_ANDROID || UNITY_IPHONE)
+            oculusMain.SetActive(false);
+            cardboardMain.SetActive(true);
+
+            NetworkManager net = GetComponent<NetworkManager>();
+            net.networkAddress = hostIP;
+            net.StartClient();
+            Debug.Log("NetworkStart line 21 reached!");
+        #else
+            oculusMain.SetActive(true);
+                cardboardMain.SetActive(false);
+            #endif
+    }
+}
